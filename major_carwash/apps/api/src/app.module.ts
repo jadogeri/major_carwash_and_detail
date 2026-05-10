26cabad14aa55e544a77345cffb501ac6e33220c
joseph adogeri <josephadogeridev@gmail.com>
2026-05-10 09:43:20 -0500

    Removed 'order' resource

--- major_carwash/apps/api/src/app.module.ts
+++ major_carwash/apps/api/src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
-import { OrderModule } from './order/order.module';
 import { BookingModule } from './booking/booking.module';
 import { VehicleModule } from './vehicle/vehicle.module';
 import { LocationModule } from './location/location.module';
@@ -10,7 +9,7 @@ import { ServiceModule } from './service/service.module';
 import { ScheduleModule } from './schedule/schedule.module';
 
 @Module({
-  imports: [UserModule, OrderModule, BookingModule, VehicleModule, LocationModule, ServiceModule, ScheduleModule],
+  imports: [UserModule, BookingModule, VehicleModule, LocationModule, ServiceModule, ScheduleModule],
   controllers: [AppController],
   providers: [AppService],
 })
